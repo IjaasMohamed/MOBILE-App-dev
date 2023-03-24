740765581e802dba4955b9dfb1d47c9765b4f4b4
IjaasMohamed <undyin0@gmail.com>
2023-03-24 02:01:51 -0300

    Daos configured

--- app/src/main/java/com/example/tutorial05/adapters/TodoAdapter.kt
+++ app/src/main/java/com/example/tutorial05/adapters/TodoAdapter.kt
@@ -6,10 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.tutorial05.R
+import com.example.tutorial05.database.TodoDatabase
 import com.example.tutorial05.database.entities.Todo
+import com.example.tutorial05.database.repositories.TodoRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class TodoAdapter: RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
 
@@ -37,13 +44,27 @@ class TodoAdapter: RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-    return 1
+    return data.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         holder.cbTodo.text = data[position].item
 
+        if(holder.cbTodo.isChecked){
+            val repository = TodoRepository(TodoDatabase.getInstance(context))
+
+            CoroutineScope(Dispatchers.IO).launch{
+                repository.delete(data[position])
+                val data = repository.getAllTodos()
+
+                withContext(Dispatchers.Main){
+                    setData(data,context)
+                }
+            }
+        }else {
+            Toast.makeText(context, "Cannot delete unchecked itens", Toast.LENGTH_LONG).show()
+        }
     }
 
 }
